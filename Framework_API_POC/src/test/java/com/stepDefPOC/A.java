8f5453bd35198656f2e5319f4144da546b235b34
frankysv0 <frankysv04@gmail.com>
2025-01-13 14:24:53 +0530

    My API Project

---
+++ Framework_API_POC/src/test/java/com/stepDefPOC/A.java
@@ -0,0 +1,18 @@
+package com.stepDefPOC;
+
+public class A {
+
+	public static void main(String[] args) {
+
+		String[] a = { "franklin", "a", "c" };
+
+		for (String s : a) {
+			System.out.println(s);
+		}
+
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ Framework_API_POC/src/test/java/com/testRunnerPOC/RunnerPOC.java
@@ -0,0 +1,16 @@
+package com.testRunnerPOC;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+/*@RunWith(Cucumber.class)
+@CucumberOptions(features = "C://Users//fselvakumar//New Eclipse 2024//Framework_API_POC//src//test//java//com//featurePOC//HotelBooking.feature", glue = {
+		"com.stepDefPOC" }, plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })*/
+public class RunnerPOC {
+
+	public static void main(String[] args) {
+
+	}
+}
