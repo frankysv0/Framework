c1beea0a96f4c3449d8f07dc927ec03971b8394d
frankysv0 <frankysv04@gmail.com>
2025-01-13 18:10:18 +0530

    Added Jenkins file

--- Framework_API_POC/src/test/java/com/testRunnerPOC/RunnerPOC.java
+++ Framework_API_POC/src/test/java/com/testRunnerPOC/RunnerPOC.java
@@ -5,9 +5,10 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-/*@RunWith(Cucumber.class)
+@RunWith(Cucumber.class)
 @CucumberOptions(features = "C://Users//fselvakumar//New Eclipse 2024//Framework_API_POC//src//test//java//com//featurePOC//HotelBooking.feature", glue = {
-		"com.stepDefPOC" }, plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" })*/
+		"com.stepDefPOC" }, plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+				"json:target/cucumber.json" })
 public class RunnerPOC {
 
 	public static void main(String[] args) {
